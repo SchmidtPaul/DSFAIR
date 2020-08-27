95ee16d8965ac6ad463d3ebba41cdb7801fa8721
Paul Schmidt <paul.schmidt@uni-hohenheim.de>
2020-08-27 16:41:31 +0200

    Revert "Intitialize page"
    This reverts commit 43337baaef040084cff863d123c05e2a5805c837.

--- 0 render_site.R
+++
@@ -1,9 +0,0 @@
-setwd("D:/Coding/DSFAIR")
-library("rmarkdown")
-
-rmarkdown::clean_site()  # delete old files
-rmarkdown::render_site(encoding="UTF-8") # render all files new; UTF-8 for ä, ö, ü, ß
-
-# get r files from rmd files
-# knitr::purl("heterogeneous_error_variance.Rmd", documentation = 0)
-
